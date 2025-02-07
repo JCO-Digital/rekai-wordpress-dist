9764d36ef15f9f010b11a77dfbc42e08486f8ec0
maxemilian-jco <maxemilian.gronblom@jco.fi>
2025-02-07 11:49:59 +0000

    Update main to output generated at 83fc5d0

--- blocks/build/qna/index.asset.php
+++ blocks/build/qna/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '83e0b44d25b28c5d3262');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b158c563c25b8fd0737b');
--- blocks/build/recommendations/index.asset.php
+++ blocks/build/recommendations/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cd8f922264ed56c9da8e');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bd143f0a02967eb5b87b');
