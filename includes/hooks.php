8cd30b6c4743ba681e9ed965cae8cdb709ea6765
foonly <niklas@uplink.fi>
2025-02-07 09:35:50 +0000

    Update main to output generated at b9c21c0

--- includes/hooks.php
+++ includes/hooks.php
@@ -31,3 +31,29 @@ add_action(
 	'admin_notices',
 	'Rekai\handle_notices'
 );
+
+add_filter( 'plugin_action_links_rekai-wordpress/rekai-wordpress.php', '\Rekai\settings_link' );
+/**
+ * Add settings link to plugin listing.
+ *
+ * @param array $links Array of plugin action links.
+ * @return array Modified array of plugin action links.
+ */
+function settings_link( $links ) {
+	// Build and escape the URL.
+	$url = esc_url(
+		add_query_arg(
+			'page',
+			'rekai-settings',
+			get_admin_url() . 'admin.php'
+		)
+	);
+	// Create the link.
+	$settings_link = "<a href='$url'>" . __( 'Settings', 'rekai-wordpress' ) . '</a>';
+	// Adds the link to the end of the array.
+	array_push(
+		$links,
+		$settings_link
+	);
+	return $links;
+}
